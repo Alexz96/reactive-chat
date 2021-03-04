a9f3e10aec2f4562a1a5dcce7ef2d35fb1b3924e
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2021-03-03 23:27:02 -0300

    Begin the project
    Start of the project with the base of the Chat App.

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+/**
+ * Basic configuration to start using Socket.io
+ */
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
