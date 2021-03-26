e73ba48af637e049ed907e5fe808bd79c06249d5
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2021-03-25 23:30:32 -0300

    Creation of Join Component
    Created the Join component and made it usable using React Hooks. Also, updated server and created a file with the Routes.

--- server/index.js
+++ server/index.js
@@ -4,11 +4,23 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 /**
  * Basic configuration to start using Socket.io
  */
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
